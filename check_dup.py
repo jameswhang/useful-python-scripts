6657991d930c03b098e674b108a89f9aa92ab6b7
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-06-13 12:38:17 -0500

    indentation was fed

--- check_dup.py
+++ check_dup.py
@@ -12,26 +12,26 @@ import hashlib, argparse, sys
 known_files = {}
 
 def md5(fname):
-	hash_md5 = hashlib.md5()
-	with open(fname, "rb") as f:
-		for chunk in iter(lambda: f.read(4096), b""):
-			hash_md5.update(chunk)
-	return hash_md5.hexdigest()
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def compare(fname1, fname2):
-	''' Takes in 2 files, compares whether they're same using md5 hashsum'''
-	return md5(fname1) == md5(fname2)
+    ''' Takes in 2 files, compares whether they're same using md5 hashsum'''
+    return md5(fname1) == md5(fname2)
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-f1', help='File 1')
-	parser.add_argument('-f2', help='File 2')
-	values = parser.parse_args()
-	if (not (values.f1 and values.f2)):
-		print 'Specify both files to compare'
-		sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f1', help='File 1')
+    parser.add_argument('-f2', help='File 2')
+    values = parser.parse_args()
+    if (not (values.f1 and values.f2)):
+        print 'Specify both files to compare'
+        sys.exit(1)
 
-	if compare(values.f1, values.f2):
-		print 'Match'
-	else:
-		print 'No match'
+    if compare(values.f1, values.f2):
+        print 'Match'
+    else:
+        print 'No match'
